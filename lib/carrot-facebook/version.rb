42729aa81f965fdbad9d750d6dd709230eed2851
Tom Milewski <tmilewski@gmail.com>
2013-02-13 15:46:48 -0500

    version

--- lib/carrot-facebook/version.rb
+++ lib/carrot-facebook/version.rb
@@ -1,12 +1,10 @@
 module Carrot
   module Facebook
-    module VERSION
-      MAJOR = 0
-      MINOR = 9
-      TINY  = 9
-      PRE   = nil
+    MAJOR = 0
+    MINOR = 9
+    TINY  = 9
+    PRE   = nil
 
-      STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
-    end
+    VERSION = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
 end
