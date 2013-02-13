8c1e2c622ab380ab27c15253b06be0e307286353
Tom Milewski <tmilewski@gmail.com>
2013-02-13 16:19:08 -0500

    remove h

--- lib/carrot-facebook/controller.rb
+++ lib/carrot-facebook/controller.rb
@@ -7,18 +7,17 @@ module Carrot
 
       def top_redirect_to(*args)
         @redirect_url = url_for(*args)
-        render :layout => false, :inline => "
+        render :layout => false, :inline => %Q{
           <html><head>
-            <script type=\"text/javascript\">
-              window.top.location.href = \"#{@redirect_url.to_json}\";
-              alert(\"test\");
+            <script type="text/javascript">
+              window.top.location.href = "#{@redirect_url.to_json}";
             </script>
             <noscript>
-              <meta http-equiv=\"refresh\" content=\"0;url=#{h @redirect_url}\" />
-              <meta http-equiv=\"window-target\" content=\"_top\" />
+              <meta http-equiv="refresh" content="0;url=#{@redirect_url}" />
+              <meta http-equiv="window-target" content="_top" />
             </noscript>
           </head></html>
-        ".html_safe
+        }.html_safe
       end
     end
   end
