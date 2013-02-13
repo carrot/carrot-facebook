d0d585d393d34ac18fc2716f223c1ea62e71c1fb
Tom Milewski <tmilewski@gmail.com>
2013-02-13 16:30:53 -0500

    consider both iframe and non-iframe apps

--- lib/carrot-facebook/controller.rb
+++ lib/carrot-facebook/controller.rb
@@ -6,18 +6,23 @@ module Carrot
       extend ActiveSupport::Concern
 
       def top_redirect_to(*args)
-        @redirect_url = url_for(*args)
-        render :layout => false, :inline => %Q{
-          <html><head>
-            <script type="text/javascript">
-              window.top.location.href = #{@redirect_url.to_json};
-            </script>
-            <noscript>
-              <meta http-equiv="refresh" content="0;url=#{@redirect_url}" />
-              <meta http-equiv="window-target" content="_top" />
-            </noscript>
-          </head></html>
-        }.html_safe
+        if request.env[:is_facebook_app]
+          @redirect_url = [ENV["FACEBOOK_APP_URL"], url_for(*args)].join
+
+          render :layout => false, :inline => %Q{
+            <html><head>
+              <script type="text/javascript">
+                window.top.location.href = #{@redirect_url.to_json};
+              </script>
+              <noscript>
+                <meta http-equiv="refresh" content="0;url=#{@redirect_url}" />
+                <meta http-equiv="window-target" content="_top" />
+              </noscript>
+            </head></html>
+          }.html_safe
+        else
+          redirect_to(*args)
+        end
       end
     end
   end
