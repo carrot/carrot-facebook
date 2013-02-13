08aab10ace928afd8bb55de61980440fe5c8f774
Tom Milewski <tmilewski@gmail.com>
2013-02-13 17:11:36 -0500

    test

--- lib/carrot-facebook/controller.rb
+++ lib/carrot-facebook/controller.rb
@@ -6,7 +6,7 @@ module Carrot
       extend ActiveSupport::Concern
 
       def top_redirect_to(*args)
-        if false #request.env[:is_facebook_app]
+        #if request.env[:is_facebook_app]
           @redirect_url = [ENV["FACEBOOK_APP_URL"], url_for(*args)].join
 
           render :layout => false, :inline => %Q{
@@ -20,9 +20,9 @@ module Carrot
               </noscript>
             </head></html>
           }.html_safe
-        else
-          redirect_to(*args)
-        end
+        #else
+        #  redirect_to(*args)
+        #end
       end
     end
   end
